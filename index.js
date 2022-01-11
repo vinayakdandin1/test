3e50643263ddaf4e98f9f377e36b62682200eb88
Vinayak Dandin <vinayakdandin1@gmail.com>
2022-01-11 23:28:00 +0100

    other changes in feature

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import React from 'react'
 function index() {
     return (
         <div>
-            <h1>Hello world inside feature branch</h1>
+            <h1>Hello world inside feature branch some changes added in feature</h1>
         </div>
     )
 }

d3fe50819aa5b5f86cf276c2335561cc62aacc1f
Vinayak Dandin <vinayakdandin1@gmail.com>
2022-01-11 23:25:46 +0100

    changes in main

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import React from 'react'
 function index() {
     return (
         <div>
-            <h1>Hello world</h1>
+            <h1>Hello world changes in main</h1>
         </div>
     )
 }
